2a46b4521695015cdf1731d8209e7c0c312ea17f
William N <root@ip-172-30-0-244.us-west-2.compute.internal>
2020-03-14 14:48:58 +0000

    Fixed kubernetes deploy script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=iamwilliam/sklearn:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment sklearn --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment sklearn --type="NodePort" --port=8000 --target-port=80
